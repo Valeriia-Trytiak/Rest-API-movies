f681464b306ccb595204720fc9123b4143dde5a7
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2024-01-09 14:08:30 +0100

    add authenticate

---
+++ middlewares/authenticate.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import { HttpError } from "../helpers/index.js";
+import User from "../models/User.js";
+
+const { JWT_SECRET } = process.env;
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers; //забираем заголовок из запроса и дальше делаем проверку существует ли он
+  if (!authorization) {
+    return next(HttpError(401, "Authorization not define")); //некст не прерівает віполнение функции, не забіть ретерн
+  }
+  const [bearer, token] = authorization.split(" "); // нужно проверить, что в заголовке первое слово - bearer, а второе - валидный токен. Используем метод строк сплит и деструктуризацию массива
+  if (bearer !== "Bearer") {
+    return next(HttpError(401)); //сообщение об ошибке, что в заголовке нет слова Bearer
+  }
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET); // если токен валидный, он выбрамывает пейлоад и нужно проверить существует ли пользователь с таким айди
+    const user = await User.findById(id);
+    if (!user) {
+      return next(HttpError(401)); //если юзера нет с таким айди, передаем ошибку, если есть - некст
+    }
+    next();
+  } catch (error) {
+    next(HttpError(401, error.message)); //сюда выкидывает ошибку если не валидный токен (не был зафиврован с помощью нашей строки или если время жизни  токена закончилось)
+  }
+};
+
+export default authenticate;
--- routes/api/movies-router.js
+++ routes/api/movies-router.js
@@ -3,10 +3,12 @@
 import express from "express";
 
 import moviesController from "../../controllers/movies-controller.js";
-import { isEmplyBody, isValidId } from "../../middlewares/index.js";
+import { isEmplyBody, isValidId, authenticate } from "../../middlewares/index.js";
 import { validateBody } from "../../decorators/index.js";
 import { movieAddSchema, movieUpdateSchema, movieUpdateFavoriteSchema } from "../../models/Movie.js";
 const moviesRouter = express.Router();
+
+moviesRouter.use(authenticate); //если одна мидлвара нужна для каждого запроса, лучше ее вынести отдельно -т.е. мы говорим экспрессу, что любой запрос сначала пройдет через этот мидлвар, если она пропустит - пройдет дальше искать брейтпоинт
 
 moviesRouter.get("/", moviesController.getAll); //получили запрос, вызвали необходимый метод json(функцию для обработки запроса), отправили ответ - вынесли эту ллогику в отдельную функцию, экспортировали как объект и вызываем методы на нужный запрос
 
